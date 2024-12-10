5a3b5519e435f7b22c61c32caec93dd1ef79988c
Katrina Marbley <kmm433@georgetown.edu>
2024-12-10 14:23:02 -0500

    Another test for python upload

---
+++ another_test.py
@@ -0,0 +1,103 @@
+# # %% [markdown]
+# # # Headers
+# # 
+# # # H1
+# # ## H2
+# # ### H3
+# # #### H4
+# # ##### H5
+# # ###### H6
+
+# # %%
+# name
+
+# # %% [markdown]
+# # My text
+# # 
+# # + My list
+# #     + Intro
+# #     + summary
+# #     + Conlcuding
+
+# %% [markdown]
+# # Q1
+
+# %%
+name = "greg"
+
+# %%
+name[1]
+
+# %% [markdown]
+# Add two spaces at the end of a line  
+# and then press enter to add a line break
+
+# %% [markdown]
+# ## Italics/Bold
+# 
+# *What* should we do?  
+# The **first** thing to do!
+# 
+
+# %% [markdown]
+# ## Block quotes
+# 
+# A wise person once said:
+# > Life is like riding a bicycle. To keep your balance you must keep moving.
+
+# %% [markdown]
+# ## Lists
+# 
+# **Departments**
+# + Accounting
+# + Sales
+# + IT
+# 
+# **Priorities**
+# 1. New opening location
+# 2. Quarterly reviews
+# 3. Summer BBQ
+
+# %% [markdown]
+# ## Add image
+# 
+# ![GU](gu_pic.jpg)
+
+# %% [markdown]
+# ## Links
+# 
+# https://worldbank.org
+# 
+# For additional information about the World Bank please click [here](https://worldbank.org).
+
+# %% [markdown]
+# ## Tables
+# 
+# Employee  | Supervisor | Office
+# ----------|------------|------
+# Lawrence  | No         | SEA
+# Frederick | No         | STL
+# Smith     | Yes        | MIA
+
+# %% [markdown]
+# ## Code blocks
+# 
+# Here add one tab to define a variable called food which takes the value pizza:
+# 
+#     food = "pizza"
+#     
+# 
+
+# %% [markdown]
+# ## Lines
+# 
+# Separate section 1 
+# 
+# ***
+# 
+# from Section 2
+
+# %%
+
+
+
